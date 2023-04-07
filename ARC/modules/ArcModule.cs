f3d7a297ded5c44abfc2b2499e6eb658b806d662
Israel Aristide <44146685+IsraelAristide@users.noreply.github.com>
2023-04-07 19:16:34 -0400

    Added Slash commands functionality and utilities commands

---
+++ ARC/modules/ArcModule.cs
@@ -0,0 +1,32 @@
+﻿using Arc;
+using Arc.Schema;
+using DSharpPlus;
+using DSharpPlus.SlashCommands;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARC.Modules
+{
+    internal abstract class ArcModule : ApplicationCommandModule
+    {
+
+        protected readonly ArcDbContext DbContext;
+        protected readonly IServiceProvider ServiceProvider;
+        protected readonly DiscordClient ClientInstance;
+        protected readonly IConfigurationRoot GlobalConfig;
+
+        protected ArcModule(string serviceName)
+        {
+            DbContext = Arc.Arc.ArcDbContext;
+            ServiceProvider = Arc.Arc.ServiceProvider;
+            ClientInstance = Arc.Arc.ClientInstance;
+            GlobalConfig = Arc.Arc.GlobalConfig;
+            Log.Logger.Information($"MODULE LOADED: {serviceName}");
+        }
+    }
+}
